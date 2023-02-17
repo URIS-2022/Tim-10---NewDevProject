f164f47aaea814a6e1606a923a68ac0b244c5c1b
markovicnatasa <markovicka12345@gmail.com>
2023-02-17 19:20:18 +0100

    Gateway

---
+++ Country/Country/Models/GatewayDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Country.Models
+{
+    public class GatewayDto
+    {
+        public string Url { get; set; }
+    }
+}
---
+++ Parcel/Parcel/Models/GatewayDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Parcel.Models
+{
+    public class GatewayDto
+    {
+        public string Url { get; set; }
+    }
+}
